192991a728820539c6b722b204268dca61f6af73
kafadarova <k.kafadarova@gmail.com>
2017-12-10 17:02:42 +0100

    Streamers added + Ajax request changed + Debugging

--- js/app.js
+++ js/app.js
@@ -1,31 +1,40 @@
 function openNewTab(evt, tabsName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabsName).style.display = "block";
-    evt.currentTarget.className += " active";
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabsName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
 //creating an object with all streamers
-var streamers= ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+const streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 //creating a variable for the API Request
-var url="https://wind-bow.gomix.me/twitch-api";
+const url = "https://wind-bow.gomix.me/twitch-api/users/";
+$(document).ready(function() {
 
+  streamers.forEach(function(streamer) {
+    let currentUrl = `${url}${streamer}?api-version=3&callback=?`;
+    //AJAX Request
+    $.ajax({
+      type: 'GET',
+      url: currentUrl,
+      async: true,
+      dataType: "jsonp",
+      success: function(data) {
+        console.log(data);
+         if(data.status !=404) {
+        $(".streamers").append(`<li><img class="img-responsive" id="streamer-logo" src=${data.logo}><a href=${data._links.self} name=${data.display_name.toLowerCase()} target="_blank">${data.display_name}</a></li><br>`);
+      }
+      }
+    });
 
-//AJAX Request
-$.ajax({
-  type: 'GET',
-  url: url,
-  async: true,
-  dataType: "jsonp",
-  success: function(data) {
-    console.log(data);
-}
+  });
+
 });
