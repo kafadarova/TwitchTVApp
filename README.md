c34633558de41f7193da1e0483f4c24d632c6cac
kafadarova <k.kafadarova@gmail.com>
2017-12-16 23:35:05 +0100

    Read me file added

---
+++ README.md
@@ -0,0 +1,6 @@
+
+User stories 
+
+The user can see whether the Free Code Camp channel is currently streaming on Twitch TV. If so it will display some details on what they are streaming. CLicking on it the user will be sent to the Twitch TV page of that streaming chanel. 
+
+I created an array with the accounts. The API was used first to display the streams and second for the users. Using For each loop I could check each object and generate the proper html.
--- js/app.js
+++ js/app.js
@@ -1,87 +1,87 @@
-//creating an object with all streamers
-const streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  //creating an object with all streamers
+  const streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-//creating a variable for the API Request
-const url = "https://wind-bow.gomix.me/twitch-api/";
+  //creating a variable for the API Request
+  const url = "https://wind-bow.gomix.me/twitch-api/";
 
 
-$(document).ready(function() {
- $("#menu-all").focus();
-  streamers.forEach(function(streamer) {
-    let currentUrl = `${url}channels/${streamer}?api-version=3&callback=?`;
+  $(document).ready(function() {
+   $("#menu-all").focus();
+    streamers.forEach(function(streamer) {
+      let currentUrl = `${url}channels/${streamer}?api-version=3&callback=?`;
 
-    //AJAX Request
-    $.ajax({
-      type: 'GET',
-      url: currentUrl,
-      async: true,
-      dataType: "jsonp",
-      success: function(data) {
-        // console.log(data);
+      //AJAX Request
+      $.ajax({
+        type: 'GET',
+        url: currentUrl,
+        async: true,
+        dataType: "jsonp",
+        success: function(data) {
+          // console.log(data);
 
-        if (data.status != 404) {
-          $(".streamers").append(`<li class="str-channel" id=${streamer}><img class="img-responsive" id="streamer-logo" src=${data.logo}><a href=${data.url} name=${data.display_name.toLowerCase()} target="_blank">${data.display_name}</a></li><br>`);
-        }
-
-        let streamersUrl = `${url}streams/${streamer}?api-version=3&callback=?`;
-        $.ajax({
-          type: 'GET',
-          url: streamersUrl,
-          async: true,
-          dataType: "jsonp",
-
-          success: function(json) {
-            if (json.stream === null) {
-              $('#' + streamer).append('<p class="status"><i class="fa fa-stop-circle"></i> Offline</p><hr>').addClass('offline');
-            } else {
-              $('#' + streamer).append('<p class="status"><i class="fa fa-play"></i> Online<br>Status: ' + json.stream.channel.status + '</p><hr>').addClass('online');
-            }
+          if (data.status != 404) {
+            $(".streamers").append(`<li class="str-channel" id=${streamer}><img class="img-responsive" id="streamer-logo" src=${data.logo}><a href=${data.url} name=${data.display_name.toLowerCase()} target="_blank">${data.display_name}</a></li><br>`);
           }
-        });
-      }
+
+          let streamersUrl = `${url}streams/${streamer}?api-version=3&callback=?`;
+          $.ajax({
+            type: 'GET',
+            url: streamersUrl,
+            async: true,
+            dataType: "jsonp",
+
+            success: function(json) {
+              if (json.stream === null) {
+                $('#' + streamer).append('<p class="status"><i class="fa fa-stop-circle"></i> Offline</p><hr>').addClass('offline');
+              } else {
+                $('#' + streamer).append('<p class="status"><i class="fa fa-play"></i> Online<br>Status: ' + json.stream.channel.status + '</p><hr>').addClass('online');
+              }
+            }
+          });
+        }
+      });
     });
+    $("#menu-offline").click(function() {
+      $('li').removeClass('selected');
+      $('li').filter('.online').addClass('hidden');
+      $(this).addClass('selected');
+      $('li').filter('.offline').removeClass('hidden');
+    });
+
+    $("#menu-all").click(function() {
+      $('li').removeClass('hidden selected');
+      $(this).addClass('selected');
+    });
+
+    $("#menu-online").click(function() {
+      $('li').removeClass('selected');
+      $('li').filter('.online').removeClass('hidden');
+      $(this).addClass('selected');
+      $('li').filter('.offline').addClass('hidden');
+    });
+
+    $('#search').keyup(function(e) {
+      e.preventDefault();
+      // Declare variables
+      var searchedInput = this.value.toLowerCase();
+      input = document.getElementById('search');
+      ul = document.getElementsByClassName('streamers');
+      li = document.getElementsByClassName('str-channel');
+
+      // Loop through all list items, and hide those who don't match the search query
+      for (i = 0; i < li.length; i++) {
+           a = li[i].getElementsByTagName("a")[0];
+           if (a.innerHTML.toLowerCase().indexOf(searchedInput) > -1) {
+               li[i].style.display = "";
+           } else {
+               li[i].style.display = "none";
+
+           }
+       }
+
+    });
+
+
+
+
   });
-  $("#menu-offline").click(function() {
-    $('li').removeClass('selected');
-    $('li').filter('.online').addClass('hidden');
-    $(this).addClass('selected');
-    $('li').filter('.offline').removeClass('hidden');
-  });
-
-  $("#menu-all").click(function() {
-    $('li').removeClass('hidden selected');
-    $(this).addClass('selected');
-  });
-
-  $("#menu-online").click(function() {
-    $('li').removeClass('selected');
-    $('li').filter('.online').removeClass('hidden');
-    $(this).addClass('selected');
-    $('li').filter('.offline').addClass('hidden');
-  });
-
-  $('#search').keyup(function(e) {
-    e.preventDefault();
-    // Declare variables
-    var searchedInput = this.value.toLowerCase();
-    input = document.getElementById('search');
-    ul = document.getElementsByClassName('streamers');
-    li = document.getElementsByClassName('str-channel');
-
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-         a = li[i].getElementsByTagName("a")[0];
-         if (a.innerHTML.toLowerCase().indexOf(searchedInput) > -1) {
-             li[i].style.display = "";
-         } else {
-             li[i].style.display = "none";
-
-         }
-     }
-
-  });
-
-
-
-
-});
